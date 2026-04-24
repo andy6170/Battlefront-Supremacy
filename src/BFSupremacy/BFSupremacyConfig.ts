b25e367b3836a0e409d9f07c873f55b6e1abe138
andy6170 <andy6170@hotmail.com>
2026-04-24 16:31:24 +0100

    config changes

--- src/BFSupremacy/BFSupremacyConfig.ts
+++ src/BFSupremacy/BFSupremacyConfig.ts
@@ -33,15 +33,33 @@ export interface GameConfig {
     readonly ticketSpeed: number;
     readonly attacker: mod.Team;
     readonly defender: mod.Team;
+    readonly baseAttackTime: number;
+    readonly bonusTimeAddition: number;
+    readonly remainingTime: number;
+    readonly team1Progress: number;
+    readonly team2Progress: number;
+    readonly team1MCOMs: number;
+    readonly team2MCOMs: number;
+    readonly team1HQHealth: number;
+    readonly team2HQHealth: number;
+    readonly elapsedTime: number;
 }
 
 export class GameConfig {
     public static readonly gameConfig: GameConfig = {
         stage: 0,
         ticketSpeed: 5,
+        baseAttackTime: 450,
+        bonusTimeAddition: 10,
+        remainingTime: 0,
         attacker: mod.GetTeam(1),
         defender: mod.GetTeam(2),
-
-
+        team1Progress: 0,
+        team2Progress: 0,
+        team1MCOMs: 2,
+        team2MCOMs: 2,
+        team1HQHealth: 100000,
+        team2HQHealth: 100000,
+        elapsedTime: 0,
     };
 }
